7c0b0fe86818eee816b4282365830b29e163d934
Nathan P. Keene <npk@txtincorporated.com>
2016-06-20 13:07:20 -0700

    All done

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -1,14 +1,17 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads, with
+  // DONE: Create the `articles` table when the controller first loads, with
   //       code that was previously in index.html.
-  // TODO: Make the function below fetch and render the articles. It should look
+  // DONE: Make the function below fetch and render the articles. It should look
   //       very similiar to code you wrote before.
   // TODO: Hide the main section elements; reveal the #articles section:
   articlesController.index = function() {
-
+    Article.createTable(function() {
+      Article.fetchAll(articleView.initIndexPage);
+    });
+    //NOTE: main section elements are already hidden and #articles revealed by routes.js
   };
-
   module.articlesController = articlesController;
 })(window);
+articlesController.index();
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,3 +1,4 @@
+(function (module) {
 // DONE: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
@@ -5,29 +6,30 @@
 
 // If you did the prework for today, you should be able to refer to your sandbox app for these TODOs.
 
-console.log('routes.js loaded'); // Debug -- verify this in the console when the page (re)loads.
+  console.log('routes.js loaded'); // Debug -- verify this in the console when the page (re)loads.
 
-// page(...
-page.base('');
+  // page(...
+  page.base('');
 
-page('/', index);
-page('/about', about);
-page('*', notFound);
-page();
+  page('/', index);
+  page('/about', aboutController.index);
+  page('*', notFound);
+  page();
 
-function index() {
-  showSection('articles');
-}
+  function index() {
+    showSection('articles');
+  }
 
-function about() {
-  showSection('about');
-}
+  function about() {
+    showSection('about');
+  }
 
-function notFound() {
-  showSection('notFound'); // not made yet
-}
+  function notFound() {
+    showSection('notFound'); // not made yet
+  }
 
-function showSection(tag) {
-  $('section').hide();
-  $('#' + tag).show();
-}
+  function showSection(tag) {
+    $('.tab-content').hide();
+    $('#' + tag).show();
+  }
+})(window);
