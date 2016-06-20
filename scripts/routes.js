c155ee8e2cffb0dd9c060f72c61cc2e2f354a398
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-20 11:36:00 -0700

    added route code

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,10 +1,33 @@
-// TODO: Configure our two routes for this app with page.js, by registering each URL your app can handle,
+// DONE: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do we call to activate page.js? Call it to run that code right here.
+// DONE: What function do we call to activate page.js? Call it to run that code right here.
 
 // If you did the prework for today, you should be able to refer to your sandbox app for these TODOs.
 
-console.log('\n* routes.js loaded *\n\n'); // Debug -- verify this in the console when the page (re)loads.
+console.log('routes.js loaded'); // Debug -- verify this in the console when the page (re)loads.
 
 // page(...
+page.base('');
+
+page('/', index);
+page('/about', about);
+page('*', notFound);
+page();
+
+function index() {
+  showSection('articles');
+}
+
+function about() {
+  showSection('about');
+}
+
+function notFound() {
+  showSection('notFound'); // not made yet
+}
+
+function showSection(tag) {
+  $('section').hide();
+  $('#' + tag).show();
+}
